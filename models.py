d37f089118c5d75d2a1058d2e92bd872bbeeb321
Justin Chung <jjkchung@gmail.com>
2023-07-25 15:39:22 -0400

    initialization

---
+++ models.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+
+
+class User(db.Model):
+    """user class"""
+
+    
