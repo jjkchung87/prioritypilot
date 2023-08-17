0f08cab8cc0234589f93957c0625403109f89226
Justin Chung <jjkchung@gmail.com>
2023-08-16 21:43:23 -0400

    debug toolbar set to dev env only

--- app.py
+++ app.py
@@ -23,7 +23,8 @@ app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 app.debug=True
-toolbar = DebugToolbarExtension(app)
+if app.config['ENV'] == 'development':
+    toolbar = DebugToolbarExtension(app)
 app.app_context().push()
 connect_db(app)
 
